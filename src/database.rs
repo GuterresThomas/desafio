5524bc0fe18390e004823aac23344a479c2eb56f
Guterresthomas <guterresmikael@gmail.com>
2023-09-22 20:06:07 -0300

    modules

---
+++ src/database.rs
@@ -0,0 +1,84 @@
+pub mod database {
+    use tokio_postgres::{Client, Error};
+    use serde::{Serialize, Deserialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct User {
+        id: i32,
+        user_document: String,
+        credit_card_token: String,
+        value: i32,
+    }
+
+    impl User {
+        fn build_user(id: i32, user_document: String, credit_card_token: String, value: i32) -> User {
+            User {
+                id,
+                user_document,
+                credit_card_token,
+                value,
+            }
+        }
+    }
+
+pub async fn add_user(
+    client: &tokio_postgres::Client,
+    user_document: &str,
+    credit_card_token: &str,
+    value: i32,
+)-> Result<(), Error>{
+    let query = format!(
+        "INSERT INTO users_rust (
+        user_document, credit_card_token, value)
+        VALUES('{}', '{}', '{}')",
+        user_document, credit_card_token, value
+    );
+    client.execute(&query, &[]).await?;
+
+    Ok(())
+}
+
+
+pub async fn read_user(client: &Client) -> Result<Vec<User>, Error> {
+    let query = "SELECT id, user_document, credit_card_token, value FROM users_rust";
+    let rows = client.query(query, &[]).await?;
+
+    let mut users = Vec::new();
+
+    for row in rows {
+        let id:i32 = row.get(0);
+        let user_document:String = row.get(1);
+        let credit_card_token:String = row.get(2);
+        let value:i32 = row.get(3);
+
+        let user = User::build_user(id, user_document, credit_card_token, value);
+        users.push(user)
+    }
+
+    Ok(users)
+}
+
+pub async fn update_user(
+    client: &Client,
+    id: i32,
+    new_value: i32,
+) -> Result<(), Error> {
+    let query = format!("UPDATE users_rust SET value = {} WHERE id = {}",
+    new_value, id);
+    client.execute(&query, &[]).await?;
+
+    Ok(())
+}
+
+pub async fn delete_user(client: &Client, user_id:i32) -> Result<(), Error> {   
+    let query = format!("DELETE FROM users_rust WHERE id = {}", user_id);
+    client.execute(&query, &[]).await?;
+
+    Ok(())
+}
+
+
+
+
+
+}
--- src/main.rs
+++ src/main.rs
@@ -4,114 +4,9 @@ use serde::{Serialize, Deserialize};
 use tokio_postgres::{Client, GenericClient};
 use tokio_postgres::{NoTls, Error};
 
+mod tcp_server;
+mod database;
 
-
-#[tokio::main]
-async fn start_tcp_server() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080").await?;
-
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            let mut buf = [0; 1024];
-
-            // In a loop, read data from the socket and write the data back.
-            loop {
-                let n = match socket.read(&mut buf).await {
-                    // socket closed
-                    Ok(n) if n == 0 => return,
-                    Ok(n) => n,
-                    Err(e) => {
-                        eprintln!("failed to read from socket; err = {:?}", e);
-                        return;
-                    }
-                };
-
-                // Write the data back
-                if let Err(e) = socket.write_all(&buf[0..n]).await {
-                    eprintln!("failed to write to socket; err = {:?}", e);
-                    return;
-                }
-            }
-        });
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    id: i32,
-    user_document: String,
-    credit_card_token: String,
-    value: i32,
-}
-
-impl User {
-    fn build_user(id: i32, user_document: String, credit_card_token: String, value: i32) -> User {
-        User {
-            id,
-            user_document,
-            credit_card_token,
-            value,
-        }
-    }
-}
-
-pub async fn add_user(
-    client: &tokio_postgres::Client,
-    user_document: &str,
-    credit_card_token: &str,
-    value: i32,
-)-> Result<(), Error>{
-    let query = format!(
-        "INSERT INTO users_rust (
-        user_document, credit_card_token, value)
-        VALUES('{}', '{}', '{}')",
-        user_document, credit_card_token, value
-    );
-    client.execute(&query, &[]).await?;
-
-    Ok(())
-}
-
-
-pub async fn read_user(client: &Client) -> Result<Vec<User>, Error> {
-    let query = "SELECT id, user_document, credit_card_token, value FROM users_rust";
-    let rows = client.query(query, &[]).await?;
-
-    let mut users = Vec::new();
-
-    for row in rows {
-        let id:i32 = row.get(0);
-        let user_document:String = row.get(1);
-        let credit_card_token:String = row.get(2);
-        let value:i32 = row.get(3);
-
-        let user = User::build_user(id, user_document, credit_card_token, value);
-        users.push(user)
-    }
-
-    Ok(users)
-}
-
-pub async fn update_user(
-    client: &Client,
-    id: i32,
-    new_value: i32,
-) -> Result<(), Error> {
-    let query = format!("UPDATE users_rust SET value = {} WHERE id = {}",
-    new_value, id);
-    client.execute(&query, &[]).await?;
-
-    Ok(())
-}
-
-pub async fn delete_user(client: &Client, user_id:i32) -> Result<(), Error> {   
-    let query = format!("DELETE FROM users_rust WHERE id = {}", user_id);
-    client.execute(&query, &[]).await?;
-
-    Ok(())
-}
 
 
 #[tokio::main] // By default, tokio_postgres uses the tokio crate as its runtime.
@@ -128,15 +23,8 @@ async fn main() -> Result<(), Error> {
         }
     });
 
-    // Now we can execute a simple statement that just returns its parameter.
-    let rows = client
-        .query("SELECT $1::TEXT", &[&"hello world"])
-        .await?;
 
-    // And then check that we got back the same string we sent over.
-    let value: &str = rows[0].get(0);
-    assert_eq!(value, "hello world");
-
+    
     /* let user_document = "exemplo";
     let credit_card_token = "token123";
     let value = 100;
@@ -149,6 +37,8 @@ async fn main() -> Result<(), Error> {
             eprintln!("Erro ao adicionar o usuário: {}", e);
         }
     } */
+
+    use database::database::read_user;
 
     match read_user(&client).await {
         Ok(users) => {
@@ -185,9 +75,9 @@ async fn main() -> Result<(), Error> {
         }
     } */
 
+    
 
     Ok(())
-
    
    
 }
